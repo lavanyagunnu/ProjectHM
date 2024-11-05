cd513b1879d2481b431bcf9a96a4418acbc45487
lavanya gunnu <lavanyagunnu01@gmail.com>
2024-11-05 07:39:26 +0000

    created docker-compose containers for web, postgress and sql

--- Dockerfile
+++ Dockerfile
@@ -4,4 +4,5 @@ WORKDIR /app
 COPY requirements.txt .
 RUN pip install -r requirements.txt
 COPY . /app
+ENV FLASK_APP=app.py 
 CMD ["flask", "run", "--host", "0.0.0.0"]
--- app.py
+++ app.py
@@ -1,5 +1,8 @@
+import os
 from flask import Flask
 from flask_smorest import Api
+  
+
 
 from db import db
 
@@ -24,7 +27,7 @@ def create_app(db_url=None):
     app.config[
         "OPENAPI_SWAGGER_UI_URL"
     ] = "https://cdn.jsdelivr.net/npm/swagger-ui-dist/"
-    app.config["SQLALCHEMY_DATABASE_URI"] = db_url or "sqlite:///data.db"
+    app.config["SQLALCHEMY_DATABASE_URI"] = db_url or os.getenv("DATABASE_URL")  
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
     app.config["PROPAGATE_EXCEPTIONS"] = True
     db.init_app(app)
